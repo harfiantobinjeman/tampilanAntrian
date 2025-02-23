d5b5578a78d7af566e537afc59b15b94e670fbb6
anto <harfiantobinjeman@gmail.com>
2025-02-23 22:23:56 +0700

    tampilan

--- src/tampilan/Api.jsx
+++ src/tampilan/Api.jsx
@@ -5,6 +5,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import Panggil from './Card';
 
 const cards = [
   {
@@ -22,89 +23,84 @@ const cards = [
     title: 'LOKET 3',
     description: 'No Antrian',
     noAntrian: 'C-1',
-  },{
-    id: 1,
-    title: 'LOKET 4',
-    description: 'No Antrian',
-    noAntrian: 'A-2',
-  },{
-    id: 1,
-    title: 'LOKET 5',
-    description: 'No Antrian',
-    noAntrian: 'B-1',
-  },
+  }
 ];
+
 
 export default function Tampilan() {
   const [selectedCard, setSelectedCard] = React.useState(0);
   return (
-  <>
-  <div style={{width:'100%' }}>
-    <Box sx={{
-      width:'100%',
-      display:'flex',
-      justifyContent:'center',
-      flexWrap:'wrap', }}>
-      <Box sx={{width:'67%', margin:'10px' }}>
-        <CardMedia
+    <div style={{ display:'flex', width:'100%',backgroundColor:'purple',alignContent:'center',height:'10000px'}}>
+      <Box sx={{ width:'70%',bgcolor:'purple', margin:'10px' }}>
+      <CardMedia
           component="video"
           image="/video.mp4"
           loop
           autoPlay muted
           sx={{
-            border:"4px solid",
+            border:"4px solid white",
             // height:'500px',
             display:'flex',
             justifyContent:'center'
             }}
         />
+        <Panggil />
       </Box>
-    {cards.map((card, index) => (
-        <Card sx={{
-          width:'30%',
-          margin:'8px',
-          height:'200px',
-          border:"4px solid #AD88C6"}}>
-          <CardActionArea
-            onClick={() => setSelectedCard(index)}
-            data-active={selectedCard === index ? '' : undefined}
-            sx={{
-              height: '100%',
-              '&[data-active]': {
-                backgroundColor: 'action.selected',
-                '&:hover': {
-                  backgroundColor: 'action.selectedHover',
+      <Box sx={{ width:'30%',bgcolor:'purple' }}>
+        {cards.map((card, index) => (
+          <Card sx={{
+            width:'90%',
+            margin:'10px',
+            height:'200px',
+            border:"4px solid #AD88C6"}}>
+            <CardActionArea
+              onClick={() => setSelectedCard(index)}
+              data-active={selectedCard === index ? '' : undefined}
+              sx={{
+                height: '100%',
+                '&[data-active]': {
+                  backgroundColor: 'action.selected',
+                  '&:hover': {
+                    backgroundColor: 'action.selectedHover',
+                  },
                 },
-              },
-            }}
-          >
-            <CardContent sx={{
-              height: '100%',
-              padding :0}}>
-              <Typography variant="h5" component="div" sx={{
-                fontSize:'40px',
-                fontWeight:'bold',
-                borderBottom:"4px solid",
-                bgcolor:'#C65BCF'}}>
-                {card.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary"sx={{
-                fontSize:'20px',
-                bgcolor:'#F8C794' }}>
-                {card.description}
-              </Typography>
-              <Typography variant="body2" color="text.secondary" sx={{
-                fontSize:'80px',
-                bgcolor:'#F8C794'}}>
-                {card.noAntrian}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        
-      ))}
-    </Box>
-  </div>
-  </>
+              }}
+            >
+              <CardContent sx={{
+                height: '100%',
+                padding :0}}>
+                <Typography variant="h5" component="div" sx={{
+                  fontSize:'40px',
+                  fontWeight:'bold',
+                  borderBottom:"4px solid",
+                  bgcolor:'#C65BCF'}}>
+                  {card.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary"sx={{
+                  fontSize:'20px',
+                  bgcolor:'#F8C794' }}>
+                  {card.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                  fontSize:'80px',
+                  bgcolor:'#F8C794'}}>
+                  {card.noAntrian}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          
+        ))}
+      </Box>
+      <marquee style={{
+          fontSize:'20px',
+          fontWeight:'bold',
+          position: 'fixed',
+          width: '100%',
+          bottom: '10px',
+          border: '3px solid #8AC007;' }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio voluptate animi iusto minima, temporibus aut harum adipisci consequuntur maiores, illum corporis nam sint laborum, nostrum eum quas. Ab, quidem delectus.
+          </marquee>
+    </div>
   );
 }
--- src/tampilan/Card.jsx
+++ src/tampilan/Card.jsx
@@ -1,99 +1,77 @@
 import * as React from 'react';
+import CardMedia from '@mui/material/CardMedia';
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Drawer from '@mui/material/Drawer';
-import Divider from '@mui/material/Divider';
-import axios from 'axios';
-// import useSWR from "swr";
+import CardActionArea from '@mui/material/CardActionArea';
 
-import PanggilanSelanjutnya from './PanggilanSelanjutnya.jsx';
+const cards = [
+  {
+    id: 1,
+    title: 'LOKET 4',
+    description: 'No Antrian',
+    noAntrian: 'A-2',
+  },{
+    id: 1,
+    title: 'LOKET 5',
+    description: 'No Antrian',
+    noAntrian: 'B-1',
+  },
+];
 
-const drawerWidth = 500;
-const socket3 = new WebSocket('wss://antrian-online.onrender.com/antrian/v1/loket/user-id');
-
-const RecipeReviewCard = ()=> {
-  const [data, setData] = React.useState([])
-  const [token, setToken] = React.useState("")
-  React.useEffect(()=>{
-    setToken(localStorage.getItem("token"))
-  },[])
-  const [dataMaster, setDataMaster] = React.useState([])
-
-   const [fetchLagi, setFetchLagi] = React.useState(false)
-      
-   
-          socket3.onclose = () => {
-              console.log('WebSocket connection closed');
-          };
-          socket3.onopen = () => {
-            console.log('WebSocket connection open');
-        };
-          socket3.onmessage = (event) => {
-              const data = JSON.parse(event.data);
-              console.log('Real-time update:', data);
-              if(data.type==="selesai" || data.type==="panggil" || data.type==="insert-antrian"){
-                  setFetchLagi(!fetchLagi)
-              }
-          };
-      
-      
-  React.useEffect(()=>{
-    axios.get("https://antrian-online.onrender.com/antrian/v1/loket/list").then(res=>{
-      setData(res?.data?.data)
-    });
-  },[fetchLagi])
-
-  React.useEffect(()=>{
-    if(token){
-    axios.get("https://antrian-online.onrender.com/antrian/v1/admin/loket/list?page=1&row_perpage=10",{headers:{"Authorization":"Bearer "+token}}).then(res=>{
-    //  console.log(res?.data?.data)
-      setDataMaster(res?.data?.data)
-    });
-  }
-  },[token])
-   
-
-console.log(data, dataMaster)
-  if (!dataMaster?.length) {
-      return <h2>loading ..... ....... ......</h2>
-  }
-
+const Panggil = () => {
+  const [selectedCard, setSelectedCard] = React.useState(0);
   return (
-    <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
-            width: drawerWidth,
-            boxSizing: 'border-box',
-            marginTop:'70px'
-          },
-        }}
-        variant="permanent"
-        anchor="right"
-      >
-        {dataMaster?.map((panggilans, index)=>(
-          <>
-            <Divider />
-              <Card sx={{ maxWidth: 430,height:200, marginTop:2, marginLeft:4,border:"4px solid black", borderRadius:10,backgroundColor:"#90caf9" }}>
-                <CardContent>
-                  <Typography gutterBottom variant="h3" component="div">
-                  <Divider/>
-                    <b style={{ color:"#fb8c00" }}>{panggilans.name}</b>
-                  <Divider/>
-                  </Typography>
-                  <Typography variant="h2" sx={{ color: 'text.secondary' }}>
-                    {data?.length && data?.findIndex(aa=>aa.name==panggilans.name)!=-1?("("+data[data.findIndex(aa=>aa.name==panggilans.name)].tipe_pasien_name+") " + data[data.findIndex(aa=>aa.name==panggilans.name)].number):""}
-                  </Typography>
-                </CardContent>
-              </Card>
-            <Divider />
-          </>
+    <div style={{ display:'flex', flexWrap:'wrap', width:'100%' }}>
+      {cards.map((card, index) => (
+          <Card sx={{
+            width:'30%',
+            margin:'10px',
+            alignContent:'center',
+            height:'200px',
+            border:"4px solid #AD88C6"}}>
+            <CardActionArea
+              onClick={() => setSelectedCard(index)}
+              data-active={selectedCard === index ? '' : undefined}
+              sx={{
+                height: '100%',
+                '&[data-active]': {
+                  backgroundColor: 'action.selected',
+                  '&:hover': {
+                    backgroundColor: 'action.selectedHover',
+                  },
+                },
+              }}
+            >
+              <CardContent sx={{
+                height: '100%',
+                padding :0}}>
+                <Typography variant="h5" component="div" sx={{
+                  fontSize:'40px',
+                  fontWeight:'bold',
+                  borderBottom:"4px solid",
+                  bgcolor:'#C65BCF'}}>
+                  {card.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary"sx={{
+                  fontSize:'20px',
+                  bgcolor:'#F8C794' }}>
+                  {card.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{
+                  fontSize:'80px',
+                  bgcolor:'#F8C794'}}>
+                  {card.noAntrian}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          
         ))}
-        <PanggilanSelanjutnya/>
-    </Drawer>
-  );
+        
+    </div>
+  )
 }
 
-export default RecipeReviewCard
+export default Panggil
