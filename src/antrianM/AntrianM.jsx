80df29b3c7c60a2ee18c36be57a7313973d23ea6
NM00690X <muzakki.ic@dikshatek.co.id>
2025-02-23 16:37:43 +0700

    print

--- src/antrianM/AntrianM.jsx
+++ src/antrianM/AntrianM.jsx
@@ -1,10 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from "axios";
+import { useReactToPrint } from "react-to-print";
 // import useSWR from "swr";
 
 const KarcisList = ()=>{
     const [data, setData] = useState([])
+  
     const [fetchLagi, setFetchLagi] = useState(false)
+
+    const componentRef = useRef(null);
+
+    const handleAfterPrint = React.useCallback(() => {
+        
+          componentRef.current.innerHTML =""
+      console.log("`onAfterPrint` called");
+    }, []);
+  
+    const handleBeforePrint = React.useCallback(() => {
+        
+      return Promise.resolve();
+    }, []);
+  
+    const printFn = useReactToPrint({
+      contentRef: componentRef,
+      documentTitle:"",
+      pageStyle:"@page { margin:0.3in;size: 2.28346in 2.28346in}  body{margin:0}",
+      onAfterPrint: handleAfterPrint,
+      onBeforePrint: handleBeforePrint,
+    });
+  
 
     // const socket = new WebSocket('ws://localhost:8000/antrian/v1/antrian/user-id?loket_id=1&token=eyJhbGciOiJFUzI1NiIsInR5cCI6ImFjY2Vzc190b2tlbiJ9.eyJleHAiOjE3Mzk3NDcxMDEsImlzcyI6ImFudHJpYW5hY2Nlc3MiLCJzdWIiOiIxIiwia2V5IjoiYW50cmlhbmFjY2VzczoxOmFjY2Vzc190b2tlbiJ9.9kGDPGIQow0kv5rGoLLXQ2VGW0TP3t0Ji1wdkpISrQqBMAOKWPuj3qFul1i362BY_jdCH-EUQ0sj1W2buMoj5g');
     const fetcher2 = async(id)=>{
@@ -25,6 +49,12 @@ const KarcisList = ()=>{
             console.log('Real-time update:', data);
             if(data.type==="selesai" || data.type==="panggil" || data.type==="insert-antrian"){
                 setFetchLagi(!fetchLagi)
+            }
+
+            if( data.type==="insert-antrian"){
+                componentRef.current.innerHTML =` <h1>${data?.data?.loket}</h1><div style="font-size:40px;font-weight:660;margin-top:-30px">${data?.data?.number}</div>`
+                
+                printFn()
             }
         };
 
@@ -87,13 +117,15 @@ const KarcisList = ()=>{
     <>
         <header className="App-header">
             
-          
+        <div ref={componentRef} style={{display:'flex',flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+       
+        </div>
             <h4 style={{ marginTop:"0px",marginBottom:"40px" }}>SILAHKAN AMBIL NO ANTRIAN DISINI</h4>
             <div className="Karcis-container">
                 {data?.map((antrians, index)=>(
                 <div onClick={()=>{fetcher2(antrians.id)}} className="Karcis-wrapper" key={antrians.id}>
                     <div className="Karcis-tittle">{antrians.name}</div>
-                    
+                   
                     
 
                 </div>
